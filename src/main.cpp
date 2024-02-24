de76ab680ad9b33ba6fb0a1636b6b09572378a2e
Kazuma Goto <oosotokara.kgoto@gmail.com>
2024-02-24 14:27:16 +0900

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 #include <M5Unified.h>
 
-#define WIDTH 80
-#define HEIGHT 160
-#define DELAY 500
+#define WIDTH 135
+#define HEIGHT 240
+#define DELAY 1000
+
+#define YAXIS_DIV_LABLE_WIDTH 40
+#define YAXIS_DIV_LABLE_HEIGHT 32
+#define YAXIS_POS_LABLE_WIDTH 40
+#define YAXIS_POS_LABLE_HEIGHT 16
+#define XAXIS_DIV_LABLE_WIDTH 80
+#define XAXIS_DIV_LABLE_HEIGHT 16
 
 M5GFX display;
 M5Canvas canvas(&display);
@@ -14,7 +21,8 @@ void setup() {
   M5.begin();
   display.init();
 
-  canvas.createSprite(10, 20);
+  // 全体領域のうち波形に割り当てる領域サイズを指定する
+  canvas.createSprite(200, 96);
 
   display.fillScreen(BLACK);
 }
@@ -29,7 +37,8 @@ void loop() {
   }
 
   display.startWrite();
-  canvas.pushSprite(8, 8);
+  canvas.setPivot(100, 48);
+  canvas.pushRotateZoom(67.5, 120, 90, 1, 1);//pushSprite(8, 8);
   display.endWrite();
 
   count++;
